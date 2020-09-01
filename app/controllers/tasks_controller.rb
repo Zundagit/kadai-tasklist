55a81643c96da48deb627a3dac869f052f7fd6a5
takanori.saito <saito1715944@gmail.com>
2020-09-01 04:55:19 +0000

    twitter-kadai1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:index, :show]
   
   def index
-      @tasks = Task.all
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.page(params[:page])
   end
 
   def show
@@ -13,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
